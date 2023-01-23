90f1c7923355cca5fbfaf8d4b36ba4c0d5436c62
Jae-Seung Lee <jaeseung@gmail.com>
2023-01-23 17:54:39 -0500

    Feature: Add protobuf

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "MTAFeed",
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v14),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,13 +18,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MTAFeed",
-            dependencies: []),
+            dependencies: [.product(name: "SwiftProtobuf", package: "swift-protobuf")]),
         .testTarget(
             name: "MTAFeedTests",
             dependencies: ["MTAFeed"]),
