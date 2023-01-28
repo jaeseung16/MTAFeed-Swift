956307c884c9bca49e21c04c24e00c59d7994a4e
Jae-Seung Lee <jaeseung@gmail.com>
2023-01-28 10:25:48 -0500

    Chore: Add minimum platform requirement

--- Package.swift
+++ Package.swift
@@ -6,23 +6,18 @@ import PackageDescription
 let package = Package(
     name: "MTAFeed",
     platforms: [
-        .macOS(.v11),
-        .iOS(.v14),
+        .macOS(.v12),
+        .iOS(.v15),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MTAFeed",
             targets: ["MTAFeed"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MTAFeed",
             dependencies: [.product(name: "SwiftProtobuf", package: "swift-protobuf")]),
