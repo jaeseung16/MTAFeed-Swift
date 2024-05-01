e5e490b6e59570a012274e8061323a7ed5a88fdc
Jae-Seung Lee <jaeseung@gmail.com>
2024-05-01 18:03:40 -0400

    Feature: Add async download(from:)

--- Sources/MTAFeed/helper/MTAFeedDownloader.swift
+++ Sources/MTAFeed/helper/MTAFeedDownloader.swift
@@ -30,6 +30,14 @@ public class MTAFeedDownloader<K> where K: MTAFeedURL {
         }
     }
     
+    public func download(from mtaFeedURL: K) async throws -> MTAFeedWrapper? {
+        var mtaFeedWrapper: MTAFeedWrapper?
+        if let urlRequest = mtaFeedURL.urlRequest(apiKey: apiKey) {
+            mtaFeedWrapper = try await download(with: urlRequest)
+        }
+        return mtaFeedWrapper
+    }
+    
     public func download(with urlRequest: URLRequest, completionHandler: @escaping (MTAFeedWrapper?, MTAFeedDownloadError?) -> Void) -> Void {
         download(with: urlRequest) { result in
             switch result {
@@ -40,6 +48,11 @@ public class MTAFeedDownloader<K> where K: MTAFeedURL {
                 completionHandler(nil, error)
             }
         }
+    }
+    
+    public func download(with urlRequest: URLRequest) async throws -> MTAFeedWrapper {
+        let feed: TransitRealtime_FeedMessage = try await download(with: urlRequest)
+        return mtaFeedProcessor.process(feed)
     }
     
     private func download(with urlRequest: URLRequest, completionHandler: @escaping (Result<TransitRealtime_FeedMessage, MTAFeedDownloadError>) -> Void) -> Void {
@@ -67,6 +80,31 @@ public class MTAFeedDownloader<K> where K: MTAFeedURL {
         }
         
         task.resume()
+    }
+    
+    private func download(with urlRequest: URLRequest) async throws -> TransitRealtime_FeedMessage {
+        logger.info("Start downloading feeds: urlRequest=\(urlRequest, privacy: .public)")
+        
+        let start = Date()
+        
+        let (data, urlResponse) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard let httpURLResponse = urlResponse as? HTTPURLResponse, httpURLResponse.statusCode == 200 else {
+            throw MTAFeedDownloadError.noData
+        }
+        
+        self.logger.debug("data = \(String(describing: data))")
+        
+        let feed = try? TransitRealtime_FeedMessage(serializedData: data, extensions: Nyct_u45Subway_Extensions)
+        
+        guard let feed = feed else {
+            logger.error("Cannot parse feed data: urlRequest=\(urlRequest, privacy: .public)")
+            throw MTAFeedDownloadError.cannotParse
+        }
+        
+        logger.log("It took \(DateInterval(start: start, end: Date()).duration) sec to download feed: urlRequest=\(urlRequest, privacy: .public)")
+        
+        return feed
     }
 
 }
